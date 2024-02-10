700e66499b41807f24724deb55180f115926c4df
IshanSingla69 <singlaishan69@gmail.com>
2024-02-10 16:27:26 +0530

    made the todolist app by myself

---
+++ todolist/home/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add_task/', views.add_task, name='add_task'),
+    path('refresh/', views.refresh, name='refresh'),
+    path('delete_task/<int:id>/', views.delete_task, name='delete_task'),
+]
---
+++ todolist/home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.http import HttpResponse
+from django.template import loader
+from .models import Task
+# Create your views here.
+def index(request):
+    # all_tasks = Task.objects.values_list('task_title', flat=True)
+    # all_taskIDs = Task.objects.values_list('task_id', flat=True)
+    # return render(request, 'home/index.html', {'tasks': all_tasks}, {'taskID': all_taskIDs})
+    template = loader.get_template('home/index.html')
+    all_tasks = Task.objects.all().values()
+    context = {
+        'tasks': all_tasks
+    }
+    return HttpResponse(template.render(context, request))
+    
+
+def refresh(request):
+    return redirect(reverse('index'))
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['task']
+        task = Task(task_title=title)
+        task.save()        
+    return redirect(reverse('refresh'))
+    
+def delete_task(request, id):
+    if request.method == 'POST':
+        id = Task.objects.filter(task_id=id)
+        id.delete()
+    return redirect(reverse('refresh'))
