294e3ea5301fc5d4bc4a813a7d23f508bc7670e6
IshanSingla69 <singlaishan69@gmail.com>
2024-03-10 23:40:10 +0530

    commit

--- todolist/home/models.py
+++ todolist/home/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     task_title = models.CharField(max_length=100)
+    
     task_id = models.AutoField(primary_key=True)
     
