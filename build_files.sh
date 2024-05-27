e39068aeac77cf2e5bba5819e890dce60479470b
IshanSingla69 <singlaishan69@gmail.com>
2024-05-27 21:23:51 +0530

    v

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
+python3.12 -m pip install -r todolist/requirements.txt
+python3.12 todolist/manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
